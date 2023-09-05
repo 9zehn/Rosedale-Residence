5017178dbe6ccdd9d42eed0805908cf629cb0a0d
Leon Gruber <ldg2134@columbia.edu>
2023-09-04 22:28:38 -0400

    first commit

---
+++ js/custom_Tgk90eVu.js
@@ -0,0 +1,12 @@
+/* introduction (#3) */
+
+  $(function(){
+  	// more settings: https://github.com/mattboldt/typed.js/#customization
+    $(".typed").typed({
+      strings: ["Students", "Families", "Everyone"],
+      typeSpeed: 50,
+      backDelay: 2500,
+      loop: true,
+      shuffle: false
+    });
+  });
